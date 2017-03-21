0e4f34ec364e59115b3cb6084415364c8c2134cc
Yoichi Ninomiya <nino15900@gmail.com>
2017-03-21 22:34:51 +0900

    update init.el

--- init.el
+++ init.el
@@ -48,13 +48,14 @@
 ;===================================
 ;; open files by xxx-mode
 ;===================================
-(add-to-list 'auto-mode-alist '("\\.h$" . c++mode))
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
 (add-to-list 'auto-mode-alist '("Makefile.*$" . makefile-gmake-mode))
 (add-to-list 'auto-mode-alist '("\\.txt$" . shell-script-mode))
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
